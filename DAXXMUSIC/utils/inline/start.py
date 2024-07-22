c3b1e4c72abcbe946f772265ba1433cc0d854d88
SkOfficial01 <123543112+SkOfficial01@users.noreply.github.com>
2024-07-22 17:59:06 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -25,11 +25,21 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(
+                text=_["S_B_13"],
+                url=f"https://t.me/Aadi29104",
+            )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_10"], url=config.SUPPORT_CHAT),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(
+                text=_["S_B_12"],
+                url=f"https://t.me/SANKI_XDX",
         ],
     ]
     return buttons
